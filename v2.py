40728a83999b12b79c954a1e0246c851ec402155
WaldperlachFabi <80318133+WaldperlachFabi@users.noreply.github.com>
2025-10-24 14:21:06 +0200

    Implement Patient and WaitingRoom classes

---
+++ v2.py
@@ -0,0 +1,68 @@
+class Patient:
+    def __init__(self, name, surname, age):
+        self.name = name
+        self.surname = surname
+        if age >= 0:
+            self.age = age
+        else:
+            self.age = 0
+        self._next = None
+
+    def set_next(self, p):
+        self._next = p
+
+    def get_next(self):
+        return self._next
+
+    def get_name(self):
+        return self.name.strip()
+
+    def get_surname(self):
+        return self.surname.strip()
+
+    def get_age(self):
+        return self.age
+
+    def get_count(self, count=0):
+        count += 1
+        if self.get_next() is None:
+            return count
+        else:
+            return self.get_next().get_count(count)
+
+
+    def add(self):
+        if self. _next is None:
+            return self
+        else:
+            return self._next.add()
+
+
+class WaitingRoom:
+    def __init__(self, size=10):
+        self._patients = []
+        self.size = size
+        self.first = None
+
+    def add(self, p: Patient):
+        if self.first is None:
+            self.first = p
+        else:
+            if self.get_count() + 1 > self.size:
+                    raise (OverflowError)
+            self.first.add().set_next(p)
+
+
+    def remove(self) -> Patient:
+        if self.first is None:
+            return None
+        else:
+            tempP = self.first
+            self.first = self.first.get_next()
+            return tempP
+
+    def get_count(self):
+        if self.first is None:
+            return 0
+        else:
+            return self.first.get_count()
